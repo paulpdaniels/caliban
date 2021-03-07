73d7eda8d012d5db21de4606eecaed2468580056
Paul Daniels <paulpdaniels@gmail.com>
2021-03-07 15:01:14 +0800

    Add client temporal codecs

--- client/src/main/scala/caliban/client/ArgEncoder.scala
+++ client/src/main/scala/caliban/client/ArgEncoder.scala
@@ -3,6 +3,9 @@ package caliban.client
 import caliban.client.__Value.{ __BooleanValue, __ListValue, __NullValue, __NumberValue, __ObjectValue, __StringValue }
 import io.circe.Json
 
+import java.time.{ Instant, LocalDate, LocalDateTime, LocalTime, OffsetDateTime, ZoneOffset, ZonedDateTime }
+import java.time.format.DateTimeFormatter
+import java.time.temporal.Temporal
 import scala.annotation.implicitNotFound
 
 /**
@@ -15,14 +18,14 @@ import scala.annotation.implicitNotFound
 Caliban needs it to know how to encode arguments of type ${A}.
 """
 )
-trait ArgEncoder[-A] {
+trait ArgEncoder[-A] { self =>
   def encode(value: A): __Value
   def typeName: String
   def optional: Boolean      = false
   def formatTypeName: String = if (optional) typeName else s"$typeName!"
 }
 
-object ArgEncoder {
+object ArgEncoder extends TemporalEncoders {
 
   implicit val int: ArgEncoder[Int] = new ArgEncoder[Int] {
     override def encode(value: Int): __Value = __NumberValue(value)
@@ -79,4 +82,55 @@ object ArgEncoder {
     override def encode(value: Json): __Value = __Value.valueDecoder.decodeJson(value).getOrElse(__NullValue)
     override def typeName: String             = "Json"
   }
+
+}
+
+trait TemporalEncoders {
+  def temporalEncoder[A <: Temporal](name: String)(f: A => __Value): ArgEncoder[A] = new ArgEncoder[A] {
+    override def encode(value: A): __Value = f(value)
+    override val typeName: String          = name
+  }
+
+  def temporalEncoderWithFormatter[A <: Temporal](name: String)(formatter: DateTimeFormatter): ArgEncoder[A] =
+    temporalEncoder[A](name)(a => __StringValue(formatter.format(a)))
+
+  implicit lazy val instantEpochEncoder: ArgEncoder[Instant] =
+    temporalEncoder("Instant")(a => __NumberValue(a.toEpochMilli))
+
+  def localDateTimeEncoderWithFormatter(formatter: DateTimeFormatter): ArgEncoder[LocalDateTime] =
+    temporalEncoderWithFormatter("LocalDateTime")(formatter)
+
+  implicit lazy val localDateTimeEncoder: ArgEncoder[LocalDateTime] =
+    localDateTimeEncoderWithFormatter(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+
+  lazy val localDateTimeEpochEncoder: ArgEncoder[LocalDateTime] =
+    temporalEncoder("LocalDateTime")(a => __NumberValue(a.toInstant(ZoneOffset.UTC).toEpochMilli))
+
+  def offsetDateTimeEncoderWithFormatter(formatter: DateTimeFormatter): ArgEncoder[OffsetDateTime] =
+    temporalEncoderWithFormatter("OffsetDateTime")(formatter)
+
+  implicit lazy val offsetDateTimeEncoder: ArgEncoder[OffsetDateTime] =
+    offsetDateTimeEncoderWithFormatter(DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+
+  def zonedDateTimeEncoderWithFormatter(formatter: DateTimeFormatter): ArgEncoder[ZonedDateTime] =
+    temporalEncoderWithFormatter("ZonedDateTime")(formatter)
+
+  implicit lazy val zonedDateTimeEncoder: ArgEncoder[ZonedDateTime] =
+    zonedDateTimeEncoderWithFormatter(DateTimeFormatter.ISO_ZONED_DATE_TIME)
+
+  def localDateEncoderWithFormatter(formatter: DateTimeFormatter): ArgEncoder[LocalDate] =
+    temporalEncoderWithFormatter("LocalDateTime")(formatter)
+
+  implicit lazy val localDateEncoder: ArgEncoder[LocalDate] =
+    localDateEncoderWithFormatter(DateTimeFormatter.ISO_LOCAL_DATE)
+
+  lazy val localDateEpochEncoder: ArgEncoder[LocalDate] =
+    temporalEncoder("LocalDate")(a => __NumberValue(a.atStartOfDay.toInstant(ZoneOffset.UTC).toEpochMilli))
+
+  implicit lazy val localTimeEncoder: ArgEncoder[LocalTime] =
+    localTimeEncoderWithFormatter(DateTimeFormatter.ISO_LOCAL_TIME)
+
+  def localTimeEncoderWithFormatter(formatter: DateTimeFormatter): ArgEncoder[LocalTime] =
+    temporalEncoderWithFormatter("LocalTime")(formatter)
+
 }
--- client/src/main/scala/caliban/client/ScalarDecoder.scala
+++ client/src/main/scala/caliban/client/ScalarDecoder.scala
@@ -2,12 +2,15 @@ package caliban.client
 
 import scala.util.Try
 import java.util.UUID
-
 import caliban.client.CalibanClientError.DecodingError
 import caliban.client.__Value._
 import io.circe.Json
 
+import java.time.{ Instant, LocalDate, LocalDateTime, LocalTime, OffsetDateTime, OffsetTime, ZonedDateTime }
+import java.time.format.DateTimeFormatter
+import java.time.temporal.Temporal
 import scala.annotation.implicitNotFound
+import scala.util.control.NonFatal
 
 /**
  * Typeclass that defines how to decode a scalar from a GraphQL response into a proper value of type `A`.
@@ -18,22 +21,22 @@ import scala.annotation.implicitNotFound
 Caliban needs it to know how to decode a scalar of type ${A}.
 """
 )
-trait ScalarDecoder[+A] {
+trait ScalarDecoder[+A] { self =>
   def decode(value: __Value): Either[DecodingError, A]
 }
 
-object ScalarDecoder {
-  implicit val int: ScalarDecoder[Int]               = {
+object ScalarDecoder extends TemporalDecoders {
+  implicit val int: ScalarDecoder[Int] = {
     case __NumberValue(value) =>
       Try(value.toIntExact).toEither.left.map(ex => DecodingError(s"Can't build an Int from input $value", Some(ex)))
-    case other                => Left(DecodingError(s"Can't build an Int from input $other"))
+    case other => Left(DecodingError(s"Can't build an Int from input $other"))
   }
-  implicit val long: ScalarDecoder[Long]             = {
+  implicit val long: ScalarDecoder[Long] = {
     case __NumberValue(value) =>
       Try(value.toLongExact).toEither.left.map(ex => DecodingError(s"Can't build a Long from input $value", Some(ex)))
-    case other                => Left(DecodingError(s"Can't build a Long from input $other"))
+    case other => Left(DecodingError(s"Can't build a Long from input $other"))
   }
-  implicit val bigInt: ScalarDecoder[BigInt]         = {
+  implicit val bigInt: ScalarDecoder[BigInt] = {
     case __NumberValue(value) =>
       Try(value.toBigIntExact).toEither.left
         .map(ex => DecodingError(s"Can't build a BigInt from input $value", Some(ex)))
@@ -41,13 +44,13 @@ object ScalarDecoder {
           case None    => Left(DecodingError(s"Can't build a BigInt from input $value"))
           case Some(v) => Right(v)
         }
-    case other                => Left(DecodingError(s"Can't build a BigInt from input $other"))
+    case other => Left(DecodingError(s"Can't build a BigInt from input $other"))
   }
-  implicit val float: ScalarDecoder[Float]           = {
+  implicit val float: ScalarDecoder[Float] = {
     case __NumberValue(value) => Right(value.toFloat)
     case other                => Left(DecodingError(s"Can't build a Float from input $other"))
   }
-  implicit val double: ScalarDecoder[Double]         = {
+  implicit val double: ScalarDecoder[Double] = {
     case __NumberValue(value) => Right(value.toDouble)
     case other                => Left(DecodingError(s"Can't build a Double from input $other"))
   }
@@ -55,23 +58,77 @@ object ScalarDecoder {
     case __NumberValue(value) => Right(value)
     case other                => Left(DecodingError(s"Can't build a BigDecimal from input $other"))
   }
-  implicit val boolean: ScalarDecoder[Boolean]       = {
+  implicit val boolean: ScalarDecoder[Boolean] = {
     case __BooleanValue(value) => Right(value)
     case other                 => Left(DecodingError(s"Can't build a Boolean from input $other"))
   }
-  implicit val string: ScalarDecoder[String]         = {
+  implicit val string: ScalarDecoder[String] = {
     case __StringValue(value) => Right(value)
     case other                => Left(DecodingError(s"Can't build a String from input $other"))
   }
-  implicit val uuid: ScalarDecoder[UUID]             = {
+  implicit val uuid: ScalarDecoder[UUID] = {
     case __StringValue(value) =>
       Try(UUID.fromString(value)).toEither.left
         .map(ex => DecodingError(s"Can't build a UUID from input $value", Some(ex)))
-    case other                => Left(DecodingError(s"Can't build a UUID from input $other"))
+    case other => Left(DecodingError(s"Can't build a UUID from input $other"))
   }
-  implicit val unit: ScalarDecoder[Unit]             = {
+  implicit val unit: ScalarDecoder[Unit] = {
     case __ObjectValue(Nil) => Right(())
     case other              => Left(DecodingError(s"Can't build Unit from input $other"))
   }
-  implicit val json: ScalarDecoder[Json]             = value => Right(__Value.valueEncoder(value))
+  implicit val json: ScalarDecoder[Json] = value => Right(__Value.valueEncoder(value))
+
+}
+
+trait TemporalDecoders {
+  private abstract class TemporalDecoder[A](name: String) extends ScalarDecoder[A] {
+    protected[this] def parseUnsafe(input: String): A
+
+    override def decode(input: __Value): Either[DecodingError, A] = input match {
+      case __StringValue(value) =>
+        try Right(parseUnsafe(value))
+        catch {
+          case NonFatal(e) =>
+            val message = e.getMessage
+            if (message.eq(null)) Left(DecodingError(s"Can't build $name from $value", innerThrowable = Some(e)))
+            else Left(DecodingError(s"Can't build $name from $value ($message)", innerThrowable = Some(e)))
+        }
+      case _ =>
+        Left(DecodingError(s"Can't build a $name from $input"))
+    }
+  }
+
+  private object TemporalDecoder {
+    def apply[T <: Temporal](name: String)(parse: String => T): TemporalDecoder[T] = new TemporalDecoder[T](name) {
+      override protected[this] def parseUnsafe(input: String): T = parse(input)
+    }
+  }
+
+  final def localDateWithFormatter(formatter: DateTimeFormatter): ScalarDecoder[LocalDate] =
+    TemporalDecoder("LocalDate")(LocalDate.parse(_, formatter))
+  final def localTimeWithFormatter(formatter: DateTimeFormatter): ScalarDecoder[LocalTime] =
+    TemporalDecoder("LocalTime")(LocalTime.parse(_, formatter))
+  final def localDateTimeWithFormatter(formatter: DateTimeFormatter): ScalarDecoder[LocalDateTime] =
+    TemporalDecoder("LocalDateTime")(LocalDateTime.parse(_, formatter))
+  final def offsetTimeWithFormatter(formatter: DateTimeFormatter): ScalarDecoder[OffsetTime] =
+    TemporalDecoder("OffsetTime")(OffsetTime.parse(_, formatter))
+  final def offsetDateTimeWithFormatter(formatter: DateTimeFormatter): ScalarDecoder[OffsetDateTime] =
+    TemporalDecoder("OffsetDateTime")(OffsetDateTime.parse(_, formatter))
+  final def zonedDateTimeWithFormatter(formatter: DateTimeFormatter): ScalarDecoder[ZonedDateTime] =
+    TemporalDecoder("ZonedDateTime")(ZonedDateTime.parse(_, formatter))
+
+  lazy val instantEpoch: ScalarDecoder[Instant] = {
+    case i: __NumberValue => Right(Instant.ofEpochMilli(i.value.toLong))
+    case value            => Left(DecodingError(s"Can't build an Instant from $value"))
+  }
+
+  implicit lazy val instant: ScalarDecoder[Instant]             = TemporalDecoder("Instant")(Instant.parse)
+  implicit lazy val localDate: ScalarDecoder[LocalDate]         = TemporalDecoder("LocalDate")(LocalDate.parse)
+  implicit lazy val localTime: ScalarDecoder[LocalTime]         = TemporalDecoder("LocalTime")(LocalTime.parse)
+  implicit lazy val localDateTime: ScalarDecoder[LocalDateTime] = TemporalDecoder("LocalDateTime")(LocalDateTime.parse)
+  implicit lazy val offsetTime: ScalarDecoder[OffsetTime]       = TemporalDecoder("OffsetTime")(OffsetTime.parse)
+  implicit lazy val zonedDateTime: ScalarDecoder[ZonedDateTime] = TemporalDecoder("ZonedDateTime")(ZonedDateTime.parse)
+  implicit lazy val offsetDateTime: ScalarDecoder[OffsetDateTime] =
+    TemporalDecoder("OffsetDateTime")(OffsetDateTime.parse)
+
 }
